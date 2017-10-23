133e8aee4526a7e7584992a1e1cdea6b9143741d
Simon Pigot <Simon.Pigot@csiro.au>
2017-10-23 18:16:28 +1100

    iBy default, locks should be on and almost eternal....

--- web/src/main/webapp/WEB-INF/classes/setup/sql/data/data-db-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/data/data-db-default.sql
@@ -655,7 +655,7 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metada
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/workflow/allowPublishInvalidMd', 'true', 2, 100003, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/workflow/automaticUnpublishInvalidMd', 'false', 2, 100004, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/workflow/forceValidationOnMdSave', 'false', 2, 100005, 'n');
-INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/lock', '-1', 1, 100006, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/lock', '100000', 1, 100006, 'n');
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/backuparchive/enable', 'false', 2, 12000, 'n');
 
