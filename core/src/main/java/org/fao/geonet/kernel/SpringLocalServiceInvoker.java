ca7734d0d5bcc50184b6462ec406eea214016841
Simon Pigot <Simon.Pigot@csiro.au>
2018-09-07 11:47:48 +1000

    Avoid spring labyrinth that doesn't work

--- core/src/main/java/org/fao/geonet/kernel/SpringLocalServiceInvoker.java
+++ core/src/main/java/org/fao/geonet/kernel/SpringLocalServiceInvoker.java
@@ -37,6 +37,10 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.servlet.mvc.method.annotation.ServletInvocableHandlerMethod;
 
+import java.util.Enumeration;
+import java.io.ObjectInputStream;
+import org.apache.commons.io.IOUtils;
+
 import org.apache.commons.lang.StringUtils;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -73,15 +77,24 @@ public class SpringLocalServiceInvoker {
 
     public Object invoke(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        HandlerExecutionChain handlerExecutionChain = requestMappingHandlerMapping.getHandler(request);
-        HandlerMethod handlerMethod = (HandlerMethod) handlerExecutionChain.getHandler();
+        Object o;
+        String requestUrl = getUrl(request);
+        if (requestUrl.startsWith("/xml.metadata.get?uuid=")) {
+           o = invoke("/api/records/"+StringUtils.substringAfterLast(requestUrl,"="));
+        } else {
+          // this absolutely indecipherable spring shit fails with NPE on the second line
+          // God knows why and unless you have grown up with spring you probably 
+          // won't ever know
+        	HandlerExecutionChain handlerExecutionChain = requestMappingHandlerMapping.getHandler(request);
+        	HandlerMethod handlerMethod = (HandlerMethod) handlerExecutionChain.getHandler();
+	
+        	ServletInvocableHandlerMethod servletInvocableHandlerMethod = new ServletInvocableHandlerMethod(handlerMethod);
+        	servletInvocableHandlerMethod.setHandlerMethodArgumentResolvers(argumentResolvers);
+        	servletInvocableHandlerMethod.setHandlerMethodReturnValueHandlers(returnValueHandlers);
+        	servletInvocableHandlerMethod.setDataBinderFactory(webDataBinderFactory);
 
-        ServletInvocableHandlerMethod servletInvocableHandlerMethod = new ServletInvocableHandlerMethod(handlerMethod);
-        servletInvocableHandlerMethod.setHandlerMethodArgumentResolvers(argumentResolvers);
-        servletInvocableHandlerMethod.setHandlerMethodReturnValueHandlers(returnValueHandlers);
-        servletInvocableHandlerMethod.setDataBinderFactory(webDataBinderFactory);
-
-        Object o = servletInvocableHandlerMethod.invokeForRequest(new ServletWebRequest(request, response), null, new Object[0]);
+        	o = servletInvocableHandlerMethod.invokeForRequest(new ServletWebRequest(request, response), null, new Object[0]);
+        }
         // check whether we need to further process a "forward:" response
         if (o instanceof String) {
           String checkForward = (String)o;
@@ -107,6 +120,24 @@ public class SpringLocalServiceInvoker {
     }
 
     /**
+     * Get url from HttpServletRequest
+     */
+    public static String getUrl(HttpServletRequest request) {
+        String requestUrl = request.getRequestURI().toString();
+        StringBuffer queryString = new StringBuffer();
+        final Enumeration<String> parameterNames = request.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+          final String paramName = parameterNames.nextElement();
+          final String paramValue = request.getParameter(paramName);
+          queryString.append(paramName+"="+paramValue);
+        }
+        if (queryString.length() > 0) {
+            requestUrl += "?"+queryString;
+        }
+        return requestUrl;
+    }
+
+    /**
      * prepareMockRequestFromUri will search for spring services that match
      * the request and execute them. Typically used for the local:// xlink
      * speed up. Accepts urls prefixed with local://<nodename> eg. 
@@ -128,4 +159,90 @@ public class SpringLocalServiceInvoker {
         }
         return request;
     }
+
+  /**
+	 * Prints the request.
+	 *
+	 * @param httpServletRequest the http servlet request
+	 */
+	private static void printRequest(final HttpServletRequest httpServletRequest) {
+		if (httpServletRequest == null) {
+			return;
+		}
+		System.out.println("----------------------------------------");
+		System.out.println("W4 HttpServletRequest");
+		System.out.println("\tRequestURL : {}"+ httpServletRequest.getRequestURL());
+		System.out.println("\tRequestURI : {}"+ httpServletRequest.getRequestURI());
+		System.out.println("\tScheme : {}"+ httpServletRequest.getScheme());
+		System.out.println("\tAuthType : {}"+ httpServletRequest.getAuthType());
+		System.out.println("\tEncoding : {}"+ httpServletRequest.getCharacterEncoding());
+		System.out.println("\tContentLength : {}"+ httpServletRequest.getContentLength());
+		System.out.println("\tContentType : {}"+ httpServletRequest.getContentType());
+		System.out.println("\tContextPath : {}"+ httpServletRequest.getContextPath());
+		System.out.println("\tMethod : {}"+ httpServletRequest.getMethod());
+		System.out.println("\tPathInfo : {}"+ httpServletRequest.getPathInfo());
+		System.out.println("\tProtocol : {}"+ httpServletRequest.getProtocol());
+		System.out.println("\tQuery : {}"+ httpServletRequest.getQueryString());
+		System.out.println("\tRemoteAddr : {}"+ httpServletRequest.getRemoteAddr());
+		System.out.println("\tRemoteHost : {}"+ httpServletRequest.getRemoteHost());
+		System.out.println("\tRemotePort : {}"+ httpServletRequest.getRemotePort());
+		System.out.println("\tRemoteUser : {}"+ httpServletRequest.getRemoteUser());
+		System.out.println("\tSessionID : {}"+ httpServletRequest.getRequestedSessionId());
+		System.out.println("\tServerName : {}"+ httpServletRequest.getServerName());
+		System.out.println("\tServerPort : {}"+ httpServletRequest.getServerPort());
+		System.out.println("\tServletPath : {}"+ httpServletRequest.getServletPath());
+
+		System.out.println("\tDispatcherType : {}"+ httpServletRequest.getDispatcherType());
+		System.out.println("");
+
+		System.out.println("\tHeaders");
+		int j = 0;
+		final Enumeration<String> headerNames = httpServletRequest.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			final String headerName = headerNames.nextElement();
+			final String header = httpServletRequest.getHeader(headerName);
+			System.out.println("\tHeader.name="+  headerName);
+			System.out.println("\tHeader.value="+ header);
+			j++;
+		}
+
+		System.out.println("\tLocalAddr : {}"+ httpServletRequest.getLocalAddr());
+		System.out.println("\tLocale : {}"+ httpServletRequest.getLocale());
+		System.out.println("\tLocalPort : {}"+ httpServletRequest.getLocalPort());
+
+		System.out.println("");
+		System.out.println("\tParameters");
+		int k = 0;
+		final Enumeration<String> parameterNames = httpServletRequest.getParameterNames();
+		while (parameterNames.hasMoreElements()) {
+			final String paramName = parameterNames.nextElement();
+			final String paramValue = httpServletRequest.getParameter(paramName);
+			System.out.println("\tParam.name="+ paramName);
+			System.out.println("\tParam.value="+ paramValue);
+			k++;
+		}
+
+		System.out.println("");
+		System.out.println("\tParts");
+		int l = 0;
+		try {
+			for (final Object part : httpServletRequest.getParts()) {
+				System.out.println("\tParts.class="+ part != null ? part.getClass() : "");
+				System.out.println("\tParts.value={}"+ part != null ? part.toString() : "");
+				l++;
+			}
+		} catch (final Exception e) {
+			System.out.println("Exception "+ e);
+		}
+
+		try {
+			System.out.println("Request Body : {}"+
+					IOUtils.toString(httpServletRequest.getInputStream(), httpServletRequest.getCharacterEncoding()));
+			System.out.println("Request Object : {}"+ new ObjectInputStream(httpServletRequest.getInputStream()).readObject());
+		} catch (final Exception e) {
+			System.out.println("Exception "+ e);
+		}
+		System.out.println("----------------------------------------");
+	}
+
 }
