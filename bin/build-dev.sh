648a0ee64c18806efd3d9a5b45a6ad4193ef7e69
Jackett, Chris (O&A, Hobart) <chris.jackett@csiro.au>
2019-11-28 16:24:20 +1100

    Removing build script and Dockerfile, and modifying docker-compose.yml.

--- bin/build-dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build --tag=docker-registry.it.csiro.au/idc/geonetwork:dev --file=Dockerfile .
-#docker build --tag=docker-registry.it.csiro.au/idc/geonetwork:dev --target dev . --file=Dockerfile --no-cache
