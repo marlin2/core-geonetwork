0f40cf53d6f2862cbbc4ee93b0269882a78d7c71
Jackett, Chris (O&A, Hobart) <chris.jackett@csiro.au>
2019-11-29 20:49:43 +1100

    Added start and stop scripts for Docker compose and swarm modes, hierarchically organised docker-compose files to mix-in compose and swarm configurations, added a development environment setup description to README.md.

---
+++ bin/start-compose-dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -f web-ui/src/main/resources/catalog/lib/bootstrap-table/dist/extensions/group-by-v2/bootstrap-table-group-by.css -a ! -f web-ui/src/main/resources/catalog/lib/bootstrap-table/dist/extensions/group-by-v2/bootstrap-table-group-by-v2.css ]; then
+    mv web-ui/src/main/resources/catalog/lib/bootstrap-table/dist/extensions/group-by-v2/bootstrap-table-group-by.css web-ui/src/main/resources/catalog/lib/bootstrap-table/dist/extensions/group-by-v2/bootstrap-table-group-by-v2.css
+fi
+
+docker-compose  -f docker-compose.yml -f docker-compose.compose.yml up -d
