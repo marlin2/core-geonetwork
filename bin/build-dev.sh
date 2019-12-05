6e8dff3dd0093eef74ed7b7e779053540f979490
Jackett, Chris (O&A, Hobart) <chris.jackett@csiro.au>
2019-12-05 14:13:31 +1100

    Removing unnecessary git steps from build script.

--- bin/build-dev.sh
+++ bin/build-dev.sh
@@ -2,13 +2,6 @@
 
 git submodule update --init --recursive \
 && git -C schemas/iso19115-3 checkout 3.4.x
-#&& git -C e2e-tests/chromedriver checkout master \
-#&& git -C web-ui/src/main/resources/catalog/lib/style/bootstrap checkout master \
-#&& git -C web-ui/src/main/resources/catalog/lib/style/font-awesome checkout master \
-#&& git -C web-ui/src/main/resources/catalog/lib/bootstrap-table checkout master \
-#&& git -C schemas/iso19139.mcp checkout master \
-#&& git -C schemas/iso19139.anzlic checkout master \
-#&& git -C schemas/iso19139.mcp-2.0 checkout master \
 
 if [ -f web-ui/src/main/resources/catalog/lib/bootstrap-table/dist/extensions/group-by-v2/bootstrap-table-group-by.css -a ! -f web-ui/src/main/resources/catalog/lib/bootstrap-table/dist/extensions/group-by-v2/bootstrap-table-group-by-v2.css ]; then
     mv web-ui/src/main/resources/catalog/lib/bootstrap-table/dist/extensions/group-by-v2/bootstrap-table-group-by.css web-ui/src/main/resources/catalog/lib/bootstrap-table/dist/extensions/group-by-v2/bootstrap-table-group-by-v2.css
