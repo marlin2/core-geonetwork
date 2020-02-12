5822396999c1974ec2c3bf6d962951358d54c2ee
Peter Shanks <peter.shanks@csiro.au>
2020-02-12 14:47:09 +1100

    added working directory to docker exec call

--- bin/build-dev.sh
+++ bin/build-dev.sh
@@ -7,8 +7,8 @@ if [ -f web-ui/src/main/resources/catalog/lib/bootstrap-table/dist/extensions/gr
     mv web-ui/src/main/resources/catalog/lib/bootstrap-table/dist/extensions/group-by-v2/bootstrap-table-group-by.css web-ui/src/main/resources/catalog/lib/bootstrap-table/dist/extensions/group-by-v2/bootstrap-table-group-by-v2.css
 fi
 
-docker run -d -t -i --rm --name geonetwork_build -w /usr/src/geonetwork --volume `pwd`:/usr/src/geonetwork --volume `pwd`/m2:/root/.m2 docker-registry.it.csiro.au/idc/geonetwork:base bash
+docker run -d -t -i --rm --name geonetwork_build -w /usr/src/geonetwork --volume /home/sha387/sourcecode/docker/core-geonetwork:/usr/src/geonetwork --volume $HOME/.m2:/root/.m2 docker-registry.it.csiro.au/idc/geonetwork:base bash
 
-docker exec -it geonetwork_build mvn clean install -DskipTests
+docker exec -w /usr/src/geonetwork -it geonetwork_build mvn clean install -DskipTests 
 
 docker stop geonetwork_build
