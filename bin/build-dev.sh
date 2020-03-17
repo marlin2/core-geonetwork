f1703f5b1124980ab50527ada4e60be9eb68a857
Jackett, Chris (O&A, Hobart) <chris.jackett@csiro.au>
2020-03-17 13:38:02 +1100

    Switching build image over to docker-registry.it.csiro.au/idc/geonetwork:dev so that maven is installed and the build will work.

--- bin/build-dev.sh
+++ bin/build-dev.sh
@@ -7,7 +7,7 @@ if [ -f web-ui/src/main/resources/catalog/lib/bootstrap-table/dist/extensions/gr
     mv web-ui/src/main/resources/catalog/lib/bootstrap-table/dist/extensions/group-by-v2/bootstrap-table-group-by.css web-ui/src/main/resources/catalog/lib/bootstrap-table/dist/extensions/group-by-v2/bootstrap-table-group-by-v2.css
 fi
 
-docker run -d -t -i --rm --name geonetwork_dev -w /usr/src/geonetwork --volume `pwd`:/usr/src/geonetwork --volume `pwd`/m2:/root/.m2 docker-registry.it.csiro.au/idc/geonetwork:base bash
+docker run -d -t -i --rm --name geonetwork_dev -w /usr/src/geonetwork --volume `pwd`:/usr/src/geonetwork --volume `pwd`/m2:/root/.m2 docker-registry.it.csiro.au/idc/geonetwork:dev bash
 
 docker exec -w /usr/src/geonetwork -it geonetwork_dev mvn clean install -DskipTests
 
